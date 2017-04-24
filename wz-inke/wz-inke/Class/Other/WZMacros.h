008db19731114c50953d7c4bee29d28d4a99c9a2
KDB <maccarduo@katahoutekiMac-mini-2.local>
2017-04-24 14:06:40 +0800

    add macros.h

---
+++ wz-inke/wz-inke/Class/Other/WZMacros.h
@@ -0,0 +1,24 @@
+//
+//  WZMacros.h
+//  wz-inke
+//
+//  Created by KDB on 2017/4/24.
+//  Copyright © 2017年 jwz. All rights reserved.
+//
+
+#ifndef WZMacros_h
+#define WZMacros_h
+
+
+
+
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define RGB(x,y,z) [UIColor colorWithRed:(x/255.0) green:(y/255.0) blue:(z/255.0) alpha:1]
+
+
+
+
+
+
+#endif /* WZMacros_h */
