1f4bd6dfd98a3c7d7471c01323490ed1c9f846b9
KDB <maccarduo@katahoutekiMac-mini-2.local>
2017-04-24 17:48:45 +0800

    begin network

---
+++ wz-inke/wz-inke/Class/Other/APIConfig.h
@@ -0,0 +1,34 @@
+//
+//  APIConfig.h
+//  wz-inke
+//
+//  Created by KDB on 2017/4/24.
+//  Copyright © 2017年 jwz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APIConfig : NSObject
+
+//信息类服务器地址
+#define SERVER_HOST @"http://service.ingkee.com"
+
+//图片服务器地址
+#define IMAGE_HOST @"http://img.meelive.cn"
+
+
+
+
+
+
+//热门直播
+#define API_HotLive @""
+
+//附近的人
+#define API_NearLive @""
+
+//广告地址
+#define API_Advertise @""
+
+
+@end
